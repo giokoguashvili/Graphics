e187620273ff8142b110b57ea203e4e47f0db169
Giorgi Koguashvili <kogo27@gmail.com>
2018-05-15 11:16:01 +0400

    added Rect with rotation

---
+++ Rotation/Form1.Designer.cs
@@ -0,0 +1,78 @@
+namespace Rotation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(828, 449);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(245, 476);
+            this.trackBar1.Maximum = 360;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(377, 56);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 544);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Rotation/Form1.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rotation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private Bitmap RectBitmap(int angle)
+        {
+            var width = this.pictureBox.Size.Width;
+            var height = this.pictureBox.Size.Height;
+
+            var rectW = 100;
+            var rectH = 100;
+
+            var x = width / 2;// - rectW / 2;
+            var y = height / 2;// - rectH / 2;
+
+            var bitmap = new Bitmap(width, height);
+
+            Func<Point, Point> to = (p) => new Point(x + p.X, y - p.Y);
+
+            Func<double, double> toRad = a => Math.PI * a / 180;
+
+            var b = new double[][]
+            {
+                new double[] { Math.Cos(toRad(angle)), Math.Sin(toRad(angle)) },
+                new double[] { Math.Cos(toRad(angle + 90)), Math.Sin(toRad(angle + 90)) },
+            };
+
+
+            Func<Point, Point> rotate = (p) => new Point(
+                                                    Convert.ToInt32(Math.Round(p.X * b[0][0] + p.Y * b[0][1])),
+                                                    Convert.ToInt32(Math.Round(p.X * b[1][0] + p.Y * b[1][1]))
+                                               );
+            Enumerable
+                .Range(0, rectW)
+                .SelectMany(i => Enumerable.Range(0, rectH).Select(j => new Point(i, j)))
+                .Select(rotate)
+                .Select(to)
+                .Select(p =>
+                {
+                    bitmap
+                        .SetPixel(p.X, p.Y, Color.Red);
+                    return true;
+                })
+                .ToList();
+
+            bitmap.SetPixel(x, y, Color.Black);
+            return bitmap;
+        }
+
+        private void Draw()
+        {
+            this.pictureBox.Image = RectBitmap(this.trackBar1.Value); 
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Draw();
+        }
+    }
+}
