8f4e3e552d944b8bde87361590b3b7af87ea1c72
Giorgi Koguashvili <kogo27@gmail.com>
2018-07-04 04:14:36 +0400

    added Mandelbrot project

---
+++ Mandelbrot/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Mandelbrot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(32, 470);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(411, 56);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(32, 532);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Minimum = 10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(411, 56);
+            this.trackBar2.TabIndex = 2;
+            this.trackBar2.Value = 10;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 659);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ Mandelbrot/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mandelbrot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw(int n, int count)
+        {
+            var b = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            b.SetPixel(100, 100, Color.Red);
+
+            var g = Graphics.FromImage(b);
+            g.DrawEllipse(new Pen(Color.Black), new RectangleF(100, 100, 200, 200));
+
+            double toRadian(double a) => Math.PI * a / 180;
+            //var count = 100;
+            //var n = 2;
+            Func<double, PointF> point = (i) =>
+            {
+                var angle = (360 / (1.0 * count)) * i;
+                var radA = toRadian(angle);
+                var x = (float)Math.Cos(radA) * 100 + 200;
+                var y = (float)Math.Sin(radA) * 100 + 200;
+                return new PointF(x, y);
+            };
+
+            Enumerable
+                .Range(0, count)
+                .Select(i =>
+                {
+                    var pF = point(i);
+
+                    pF.X -= 2;
+                    pF.Y -= 2;
+                    g.DrawEllipse(new Pen(Color.Red), new RectangleF(pF, new Size(4, 4)));
+
+                    g.DrawLine(new Pen(Color.Green), point(i), point(i * n));
+                    return true;
+                })
+                .ToList();
+
+            this.pictureBox1.Image = b;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Draw(this.trackBar1.Value, this.trackBar2.Value);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Draw(this.trackBar1.Value, this.trackBar2.Value);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Draw(this.trackBar1.Value, this.trackBar2.Value);
+        }
+    }
+}
